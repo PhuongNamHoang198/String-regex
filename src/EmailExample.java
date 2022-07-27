51e074859349c6fe7477803ec485e62eff1fcba7
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-07-27 15:38:10 +0700

    nop bai

---
+++ src/EmailExample.java
@@ -0,0 +1,31 @@
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExample {
+    private  static Pattern pattern;
+    private  Matcher matcher;
+
+//    private static Pattern pattern;
+//    private Matcher matcher;
+//
+    private static final java.lang.String EMAIL_REGEX =   "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    public EmailExample() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+
+    }
+    public  boolean validate (String regex) {
+        matcher = pattern.matcher((CharSequence) regex);
+        return matcher.matches();
+    }
+//
+//    public EmailExample() {
+//        pattern = Pattern.compile(EMAIL_REGEX);
+//    }
+//
+//    public boolean validate(String regex) {
+//        matcher = pattern.matcher((CharSequence) regex);
+//        return matcher.matches();
+//    }
+}
